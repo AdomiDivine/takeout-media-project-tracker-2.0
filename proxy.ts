01cd6467dceffbeb4620aa01e55a85d81764f356
AdomiDivine <dannybabs123456@gmail.com>
2026-05-25 07:56:12 +0100

    Allow forgot-password and reset-password routes without auth

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
-const PUBLIC_ROUTES = ["/login", "/signup"];
+const PUBLIC_ROUTES = ["/login", "/signup", "/forgot-password", "/reset-password"];
 
 export async function proxy(request: NextRequest) {
   let supabaseResponse = NextResponse.next({ request });
